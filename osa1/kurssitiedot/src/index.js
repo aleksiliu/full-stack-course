2d34114a9c6e0c00fbeda8c59d5164c480d748fe
Aleksi <liukkonenaleksi@gmail.com>
2019-03-17 12:13:39 +0200

    Refactor to use one big object

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 
 
 const Header = props => {
+
   return (
     <h1>{props.title}</h1>
   )
 }
 
 const Content = props => {
-
   return (
     <>
       <Part part={props.parts[0].name} exerciseCount={props.parts[0].exercises} />
@@ -28,34 +28,37 @@ const Part = props => {
 }
 
 const Total = props => {
+  console.log(props)
   return (
     <p>yhteensä {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises} tehtävää</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const parts = [
-    {
-      name: 'Reactin perusteet',
-      exercises: 10
-    },
-    {
-      name: 'Tiedonvälitys propseilla',
-      exercises: 7
-    },
-    {
-      name: 'Komponenttien tila',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header title={course} />
-      <Content parts={parts}/> 
-      <Total exercises={parts} />
+      <Header title={course.name} />
+      <Content parts={course.parts}/> 
+      <Total exercises={course.parts} />
     </div>
   )
 }
