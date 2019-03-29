45f73856c51af9b7c9419bccee3276e6eb862223
Aleksi <liukkonenaleksi@gmail.com>
2019-03-29 18:08:42 +0200

    Add feature for adding names

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
+
+  const handleChange = e => {
+    setNewName(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+      id: persons.length + 1
+    };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          nimi: <input />
+          nimi: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      ...
+      <div>
+        {persons.map(person => (
+          <p key={person.id}>{person.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
