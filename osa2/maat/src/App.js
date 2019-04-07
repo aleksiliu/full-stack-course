4c22d941040fd6a75b03f9497f620f57c9072839
Aleksi <liukkonenaleksi@gmail.com>
2019-04-07 12:42:16 +0300

    Add render logic

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -5,30 +5,67 @@ const App = () => {
   const [country, setCountry] = useState('');
   const [countries, setCountries] = useState([]);
 
+  const CountryList = () => {
+    if (countries.length > 10) {
+      return <p> Too many options</p>;
+    } else if (countries.length === 1) {
+      return (
+        <div>
+          {countries.map(country => (
+            <div key={country.numericCode}>
+              <h2>{country.name}</h2>
+              <p> capital {country.population}</p>
+              <p> population {country.capital}</p>
+              <h3>languages</h3>
+              <>
+                {country.languages.map(country => (
+                  <li key={country.name}>{country.name}</li>
+                ))}
+              </>
+              <img
+                src={country.flag}
+                alt={country.flag}
+                style={{ width: 200, height: 200, marginTop: 20 }}
+              />
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {countries.map(country => (
+            <p key={country.numericCode}>{country.name}</p>
+          ))}
+        </div>
+      );
+    }
+  };
+
   useEffect(
     () => {
       const fetchData = async () => {
         const result = await axios('https://restcountries.eu/rest/v2/all');
 
-        setCountries(result.data);
+        setCountries(result.data.filter(x => x.name.toLowerCase().includes(country.toLowerCase())));
       };
 
       fetchData();
     },
-    [],
+    [country],
     console.log(countries)
   );
+
+  const handleChange = event => {
+    setCountry(event.target.value);
+  };
 
   return (
     <div>
       <h2>Maat</h2>
-      <input type="text" value={country} onChange={e => setCountry(e.target.value)} />
+      <input type="text" value={country} onChange={handleChange} />
       <div>
-        {countries
-          .filter(person => person.name.toLowerCase().includes(country.toLowerCase()))
-          .map(country => (
-            <p key={country.numericCode}>{country.name}</p>
-          ))}
+        <CountryList />
       </div>
     </div>
   );
